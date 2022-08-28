3d1c0ccad7efa53663191e4f4e41dd81c584ad94
Akramjon <akramjon00175@gmail.com>
2022-08-27 20:01:18 -0700

    Initial Commit

---
+++ GitHubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubDemo2
+//
+//  Created by Akramjon on 27/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubDemo2/GitHubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemo2App.swift
+//  GitHubDemo2
+//
+//  Created by Akramjon on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
